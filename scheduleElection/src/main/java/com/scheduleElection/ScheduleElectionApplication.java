9862c9244355465dcdd24cc26860a879fbfcb835
stos <saulo.pitang@gmail.com>
2021-09-11 07:17:05 -0300

    first commit.

---
+++ scheduleElection/src/main/java/com/scheduleElection/ScheduleElectionApplication.java
@@ -0,0 +1,13 @@
+package com.scheduleElection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleElectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleElectionApplication.class, args);
+	}
+
+}
---
+++ scheduleElection/src/test/java/com/scheduleElection/ScheduleElectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduleElection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleElectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
